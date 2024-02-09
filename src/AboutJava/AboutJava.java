5a85099ce794e707c1d5dfa6dd13978aa16574b3
Cyanoptilas <februarys22@gmail.com>
2024-02-09 13:18:56 +0900

    ADD 01 Javaプログラムの流れ

---
+++ src/AboutJava/AboutJava.java
@@ -0,0 +1,18 @@
+package AboutJava;
+
+public class AboutJava {
+    public static void test() {
+        System.out.println("01 ABout Java");
+        System.out.println("ソースファイル");
+        System.out.println("↓実行時コンパイル");
+        System.out.println("中間コード(Classファイル)が作成される");
+        System.out.println("↓");
+        System.out.println("JVMがClassファイルを読む");
+        System.out.println("↓");
+        System.out.println("ネイティブコード作成");
+        System.out.println("↓");
+        System.out.println("各OSで実行");
+        System.out.println("(ネイティブコードはファイルに保存されることなくそのまま直接実行される)");
+
+    }
+}
--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import AboutJava.AboutJava;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Stared App.java");
+
+        // 01
+        AboutJava.test();
     }
 }
